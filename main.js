60fc4b2afeee3d4cf057503ae93f9b317ef2e9d0
romakh-i <ivanromah@gmail.com>
2019-01-13 18:49:28 +0200

    responsive

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
           return a.positionNumber - b.positionNumber;
         });
 
+        // appends navbar to page
         $("body").append(`
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark navbar-center">
           <a class="navbar-brand" href="#">Categories</a>
@@ -37,14 +38,8 @@ $(document).ready(function () {
 
             $("#tabs-content").append(`<div id="cat${category.id}" class="tab-pane fade" role="tabpanel" aria-labelledby="cat${category.id}-tab"></div>`)
 
-            //$(`#tabs-content #cat${category.id}`).append(`<div class="container-fluid py-3"></div>`);
-            //const contId = "cat" + category.id + "con";
-            // $("body").append(`<div id=${contId} class="container"><ul></ul></div>`);
-            // $("#" + contId).hide();
-
             for (const item of sortedItems) {
               if (category.items.includes(item.id)) {
-                //showItem(item, contId);
                 $(`#tabs-content #cat${category.id}`).append(setContent(item));
               }
             }
@@ -55,94 +50,80 @@ $(document).ready(function () {
             $("#tabs-content").append(`<div id="cat${category.id}" class="tab-pane fade disabled" role="tabpanel" aria-labelledby="cat${category.id}-tab"></div>`)
           }
         }
-        $("#tabs div.navbar-nav a:not(.disabled):first").addClass("show active"); //.tab("show");
+        $("#tabs div.navbar-nav a:not(.disabled):first").addClass("show active");
         $("#tabs-content div:not(.disabled):first").addClass("show active");
 
         // if categories disabled
       } else {
-        const contId = "cat0con";
-        $("body").append(`<div id="${contId}" class="container"><ul></ul></div>`);
+        $("body").append('<div id="tabs-content" class="tab-content"></div>');
+
         for (const item of sortedItems) {
-          showItem(item, contId);
+          $("#tabs-content").append(setContent(item));
         }
       }
 
-      // switch category
-      $("#tabs li a").click(function () {
-        const contId = "#" + $(this).attr("id") + "con";
-        if ($(this).hasClass("not-active")) {
-          $("#tabs li a").addClass("not-active");
-          $(".container").hide();
-
-          $(this).removeClass("not-active");
-          $(contId).fadeIn("slow");
-        } else {
-          $(this).addClass("not-active");
-          $(contId).hide()
-        }
+      // hide navbar when category was choosed
+      $(".nav-item.nav-link:not(.disabled)").click(function () {
+        $(".navbar-toggler-icon").click();
       });
 
-      // open modal window about item
-      $(".container ul li").click(function () {
-        const itemId = parseInt($(this).attr("id").slice(3, 6));
+      // render modal window
+      $(".card .row").click(function () {
+        const itemId = parseInt($(this).attr("id").slice(4))
         const item = data.items.find((elem) => {
-          return elem.id == itemId
+          return elem.id == itemId;
         });
 
-        $(".modal")[0].style.display = "block";
+        $("#modal").modal("show");
 
-        $("#itemTitle")[0].innerText = item.title;
+        $("#modalTitle")[0].innerText = item.title;
         $("#longDesc")[0].innerHTML = item.long_description.replace(/(http[^\s]+)/g, "<a href='$1'>$1</a>").replace(/(\n)/g, "<br>");
-        $(".gallery-img")[0].src = item.gallery_images[0].url;
-        const photoPath = getPhotoPath();
-        $(".photoNumber")[0].innerText = getPhotoNumber(data, $("#itemTitle")[0].innerText, photoPath);
+
+        $(".carousel-indicators")[0].innerHTML = `<li data-target="#carousel" data-slide-to="0" class="active"></li>`;
+        $(".carousel-inner")[0].innerHTML = `<div class="carousel-item active">
+          <img class="gallery-img" src="${item.gallery_images[0].url}">
+        </div>`;
+
+        loadPhotos(item);
 
         // shows video if exist
         if (item.videoUrl) {
           const url = item.videoUrl.match(/\/\w+$/)[0];
-          const video = $(".video")[0];
 
-          video.src = "https://www.youtube.com/embed" + url;
-          video.style.display = "block";
+          $(".video").show();
+          $("#video")[0].src = "https://www.youtube.com/embed" + url;
         }
+      });
+
+      // load all photos into carousel
+      function loadPhotos(item) {
+        if (item.gallery_images.length > 1) {
+          const indicators = $(".carousel-indicators");
+          var inner = $(".carousel-inner");
+
+          for (let i = 1; i < item.gallery_images.length; i++) {
+            indicators.append(`<li data-target="#carousel" data-slide-to="${i}"></li>`)
+            inner.append(`<div class="carousel-item"><img class="gallery-img" src="${item.gallery_images[i].url}"></div>`);
+          }
+
+          $(".carousel-control-prev").show();
+          $(".carousel-control-next").show();
+        } else {
+          $(".carousel-control-prev").hide();
+          $(".carousel-control-next").hide();
+        }
+      }
+
+      // unload video when modal closed
+      $('#modal').on('hidden.bs.modal', function () {
+        $("#video")[0].src = "";
+        $(".video").hide();
       })
-
-      // switches to next photo
-      $(".next").click(function () {
-        const photoPath = getPhotoPath();
-        const nextPhoto = getNextPhoto(data, $("#itemTitle")[0].innerText, photoPath);
-
-        $(".gallery-img")[0].src = nextPhoto;
-        $(".photoNumber")[0].innerText = getPhotoNumber(data, $("#itemTitle")[0].innerText, nextPhoto);
-      });
-      // switches to previous photo
-      $(".prev").click(function () {
-        const photoPath = getPhotoPath();
-        const prevPhoto = getPrevPhoto(data, $("#itemTitle")[0].innerText, photoPath);
-
-        $(".gallery-img")[0].src = prevPhoto;
-        $(".photoNumber")[0].innerText = getPhotoNumber(data, $("#itemTitle")[0].innerText, prevPhoto);
-      });
     }
   });
-
-  // close the modal window by clicking on X button
-  $(".close").click(function () {
-    $(".modal")[0].style.display = "none";
-    const video = $(".video")[0];
-    video.src = "";
-    video.style.display = "none";
-  });
 })
 
-// close the modal window by clicking anywhere outside
-$(window).click(function (event) {
-  let modal = $(".modal")[0];
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-})
-
+// page main parameters initialization 
 function initPage(data) {
   $(document)[0].title = data.name;
   $("body")[0].style.color = "#" + data.accentColor;
@@ -151,10 +132,11 @@ function initPage(data) {
   $("#favicon")[0].href = "./img/Home-Icon_thumb.png"
 }
 
+// render item with its content
 function setContent(item) {
-  return `<div class="container-fluid py-3 item-content">
-    <div id="item${item.id}" class="card">
-      <div class="row ">
+  return `<div class="container-fluid py-3 item">
+    <div class="card">
+      <div class="row" id="item${item.id}">
         <div class="col-sm-4">
           <img src="${item.gallery_images[0].url}" class="small-img img-thumbnail">
         </div>
@@ -167,63 +149,4 @@ function setContent(item) {
       </div>
     </div>
   </div>`;
-}
-
-// shows one item
-function showItem(item, contId) {
-  const itemId = "cat" + item.id + "item";
-
-  $("#" + contId + " ul").append(`<li id=${itemId}></li>`);
-
-  $("#" + itemId).append(`<img src="${item.gallery_images[0].url}" class="small-img"/>`);
-  $("#" + itemId).append(`<h4 class="title">${item.title}</h4>`);
-  $("#" + itemId).append(`<p class="desc">${item.description}</p>`);
-}
-// gets photo path without root directory
-function getPhotoPath() {
-  return $(".gallery-img")[0].src.match(/img\/items\/.+$/);
-}
-// get number of current photo from all
-function getPhotoNumber(data, title, currPhotoUrl) {
-  const images = getItemByTitle(data, title).gallery_images;
-  if (images.length == 1)
-    return null;
-
-  for (let i = 0; i < images.length; i++) {
-    if (images[i].url == currPhotoUrl) {
-      return (i + 1) + "/" + images.length;
-    }
-  }
-}
-// return url of next photo
-function getNextPhoto(data, title, currPhotoUrl) {
-  const images = getItemByTitle(data, title).gallery_images;
-  if (images.length == 1) return currPhotoUrl;
-
-  for (let i = 0; i < images.length; i++) {
-    if (images[i].url == currPhotoUrl) {
-      if (i != images.length - 1) {
-        return images[i + 1].url;
-      } else return images[0].url;
-    }
-  }
-}
-// return url of previous photo
-function getPrevPhoto(data, title, currPhotoUrl) {
-  const images = getItemByTitle(data, title).gallery_images;
-  if (images.length == 1) return currPhotoUrl;
-
-  for (let i = 0; i < images.length; i++) {
-    if (images[i].url == currPhotoUrl) {
-      if (i != 0) {
-        return images[i - 1].url;
-      } else return images[images.length - 1].url;
-    }
-  }
-}
-// retrieve item by its description
-function getItemByTitle(data, title) {
-  return data.items.find((elem) => {
-    return elem.title == title;
-  })
 }
